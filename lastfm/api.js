b0fdde9cd29fa85f97af02069a45df320ba7631e
Jamie Mason <siunmaru@gmail.com>
2013-01-03 22:27:05 +0000

    lastfm api config

---
+++ lastfm/api.js
@@ -0,0 +1,16 @@
+var developer = require('../developer');
+
+exports.method = 'GET';
+exports.key = developer.lastfm.key;
+exports.secret = developer.lastfm.secret;
+exports.protocol = 'http://';
+exports.hostname = 'ws.audioscrobbler.com';
+exports.defaults = {
+  limit: 50
+};
+exports.services = {
+  user: {
+    getInfo: '/2.0/?method=user.getinfo&user=#{username}&api_key=#{apiKey}&format=json',
+    getTracks: '/2.0/?method=library.gettracks&user=#{username}&api_key=#{apiKey}&format=json&page=#{page}&limit=#{limit}'
+  }
+};
