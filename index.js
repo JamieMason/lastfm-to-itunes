9d66f07517e5078a32cebfec967af02f2573ec8d
Jamie Mason <siunmaru@gmail.com>
2013-01-06 22:10:04 +0000

    early dummy requests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var lastfm = require('./lastfm');
+var fs = require('fs');
 
+/*
 lastfm.user.getInfo('NakedIntruder', {
   end: function(data) {
     var date = new Date();
@@ -13,5 +15,39 @@ lastfm.user.getTracks('NakedIntruder', {
   limit: 5,
   end: function(data) {
     console.log(JSON.stringify(data));
+  }
+});
+
+lastfm.user.getLibrary('NakedIntruder', {
+  end: function(data) {
+    console.log(JSON.stringify(data));
+  }
+});
+*/
+
+// Download library to JSON
+// lastfm.user.getLibrary('NakedIntruder', {
+//   end: function(data) {
+//     fs.writeFile('/Users/jdog/Desktop/library.json', JSON.stringify(data), function(err) {
+//       if(err) {
+//         throw err;
+//       }
+//       console.log('SAVED: /Users/jdog/Desktop/library.json');
+//     });
+//   }
+// });
+
+lastfm.user.getLovedTracks('NakedIntruder', {
+  end: function(data) {
+    data[0].lovedtracks.track.forEach(function (track) {
+      console.log(track.artist.name + ' - ' + track.name);
+    });
+
+    // fs.writeFile('/Users/jdog/Desktop/loved.json', JSON.stringify(data), function(err) {
+    //   if(err) {
+    //     throw err;
+    //   }
+    //   console.log('SAVED: /Users/jdog/Desktop/loved.json');
+    // });
   }
 });
